9b13715cfb0be8b8ea8ad64c7e7dff1591c6ec43
Satveerk31 <satveerk2006@gmail.com>
2026-07-31 10:29:05 +0530

    Improve greeting program by adding user input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Hello darling!!!";
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello, " << name << "! Welcome to C++." << endl;
+
+    return 0;
 }
